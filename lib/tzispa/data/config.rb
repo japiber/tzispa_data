db282bb5d103376ef3863b0e4c901297675dd14a
Juan Antonio Piñero <japinero@area-integral.com>
2017-12-31 10:31:03 +0100

    moved from tzispa gem

---
+++ lib/tzispa/data/config.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'tzispa/config/yaml'
+
+module Tzispa
+  module Data
+
+    class Config
+      attr_reader :env, :config
+
+      CONFIG_FILENAME = 'database'
+
+      def initialize(env)
+        @cftime = nil
+        @env = env.to_sym
+      end
+
+      def to_h
+        load!
+        config.to_h[env]&.to_h
+      end
+
+      def load!
+        if @cftime.nil?
+          @cftime = File.ctime(filename)
+        elsif @cftime != File.ctime(filename)
+          @config = nil
+          @cftime = File.ctime(filename)
+        end
+        @config ||= Tzispa::Config::Yaml.load(filename)
+      end
+
+      def filename
+        Config.filename
+      end
+
+      class << self
+        def filename
+          @filename ||= "config/#{CONFIG_FILENAME}.yml"
+        end
+
+        def create_default(path)
+          hcfg = {}.tap do |cfg|
+            cfg['development'] = ''
+            cfg['deployment'] = ''
+            cfg['test'] = ''
+          end
+          Yaml.save(hcfg, File.join(path, filename))
+        end
+
+        def add_repository(name, adapter, dbconn)
+          hs = YAML.safe_load(File.open(filename))
+          hs.each_value do |v|
+            v[name] = {
+              'adapter' => adapter,
+              'database' => dbconn,
+              'connection_validation' => 'No',
+              'local' => 'Yes'
+            }
+          end
+          Yaml.save(hs, filename)
+        end
+      end
+    end
+
+  end
+end
