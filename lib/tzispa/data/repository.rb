46fdace6c1c395d112013fedecbac18f778c2367
Juan Antonio Piñero <japinero@area-integral.com>
2017-12-31 10:32:11 +0100

    make repository a singleton

--- lib/tzispa/data/repository.rb
+++ lib/tzispa/data/repository.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
+require 'forwardable'
 require 'sequel'
 require 'tzispa_utils'
 require 'tzispa/data/adapter_pool'
+require 'tzispa/data/config'
 
 module Tzispa
   module Data
@@ -18,62 +20,70 @@ module Tzispa
     class UnknownModel < DataError; end
 
     class Repository
+      extend Forwardable
+      include Singleton
       using Tzispa::Utils::TzString
 
-      attr_reader :root, :adapters
+      attr_reader :adapters, :pool, :config
+      def_delegators :@adapters, :disconnect
 
-      def initialize(config, root = nil)
-        @config = config
-        @root = root || LOCAL_REPO_ROOT
+      def initialize
+        @config = Config.new(Tzispa::Environment.environment)&.to_h
         @pool = {}
         @adapters = AdapterPool.new config
       end
 
-      def [](model, repo_id = nil)
-        selected_repo = repo_id || adapters.default_repo
-        raise UnknownModel.new(model, selected_repo) unless pool.key?(selected_repo) &&
-                                                            pool[selected_repo].key?(model.to_sym)
-        pool[selected_repo][model.to_sym]
-      end
+      class << self
+        def [](model, repo_id = nil)
+          repo = active_repo(repo_id)
+          raise UnknownModel.new(model, repo) unless known?(model, repo)
+          instance.pool[repo][model.to_sym]
+        end
 
-      def models(repo_id = nil)
-        pool[repo_id || adapters.default_repo].values
-      end
+        def disconnect
+          instance.disconnect
+        end
 
-      def module_const(repo_id = nil)
-        selected_repo = repo_id || adapters.default_repo
-        pool[selected_repo][:__repository_module] ||= repository_module(selected_repo)
-      end
+        def active_repo(repo_id = nil)
+          repo_id || instance.adapters.default_repo
+        end
 
-      def load!(domain)
-        @config.each do |id, cfg|
-          Mutex.new.synchronize do
-            pool[id] = {}
-            load_config_repo(id, cfg)
+        def known?(model, repo)
+          instance.pool.key?(repo) && instance.pool[repo].key?(model.to_sym)
+        end
+
+        def models(repo_id = nil)
+          instance.pool[active_repo(repo_id)].values
+        end
+
+        def module_const(repo_id = nil)
+          repo = active_repo(repo_id)
+          instance.pool[repo][:__repository_module] ||= instance.repository_module(repo)
+        end
+
+        def load!(domain)
+          instance.config.each do |id, cfg|
+            instance.pool[id] = {}
+            instance.load_config_repo(id, cfg)
             domain.include module_const(id)
           end
         end
-        self
-      end
 
-      def register(model_id, model_class, repo_id, config)
-        unless pool[repo_id][model_id.to_sym]
-          unless model_class.db == adapters[repo_id]
-            model_class.db = adapters[repo_id]
-            if config.caching
-              model_class.plugin :caching, adapters.cache[repo_id],
-                                 ttl: config.ttl || DEFAULT_CACHE_TTL,
-                                 ignore_exceptions: true
-            end
-          end
-          pool[repo_id][model_id.to_sym] = model_class
+        def register(model_id, model_class, repo_id, config)
+          return if known?(model_id, repo_id)
+          instance.setup_model(model_class, repo_id, config)
+          instance.pool[repo_id][model_id.to_sym] = model_class
         end
-        pool[repo_id][model_id.to_sym]
       end
 
-      private
-
-      attr_reader :pool
+      def setup_model(model_class, repo_id, config)
+        return if model_class.db == adapters[repo_id]
+        model_class.db = adapters[repo_id]
+        return unless config.caching
+        model_class.plugin :caching, adapters.cache[repo_id],
+                           ttl: config.ttl || DEFAULT_CACHE_TTL,
+                           ignore_exceptions: true
+      end
 
       def repository_module(repo_id)
         rm = @pool[repo_id].first[1].name.split('::')
@@ -83,29 +93,40 @@ module Tzispa
 
       def load_config_repo(id, cfg)
         if cfg.local
-          load_local_helpers id
-          load_local_models id, cfg
-          load_local_entities id
+          local_local(id, cfg)
         else
           require cfg.gem
           repo_module = id.to_s.camelize.constantize
           self.class.include repo_module
-          self.class.send "load_#{id}", self, id, cfg
+          self.class.send "load_#{id}", id, cfg
         end
       end
 
-      def load_local_models(repo_id, config)
+      private
+
+      def root(cfg)
+        cfg.root || LOCAL_REPO_ROOT
+      end
+
+      def local_local(id, cfg)
+        local_root = root(cfg)
+        load_local_helpers id, local_root
+        load_local_models id, cfg, local_root
+        load_local_entities id, local_root
+      end
+
+      def load_local_models(repo_id, cfg, root)
         models_path = "./#{root}/#{repo_id}/model"
         repo_module = "#{repo_id.to_s.camelize}::Model".constantize
         Dir["#{models_path}/*.rb"].each do |file|
           model_id = file.split('/').last.split('.').first
           require "#{models_path}/#{model_id}"
           model_class = "#{repo_module}::#{model_id.camelize}".constantize
-          register model_id, model_class, repo_id, config
+          register model_id, model_class, repo_id, cfg
         end
       end
 
-      def load_local_entities(repo_id)
+      def load_local_entities(repo_id, root)
         entities_path = "./#{root}/#{repo_id}/entity"
         Dir["#{entities_path}/*.rb"].each do |file|
           entity_id = file.split('/').last.split('.').first
@@ -113,7 +134,7 @@ module Tzispa
         end
       end
 
-      def load_local_helpers(repo_id)
+      def load_local_helpers(repo_id, root)
         helpers_path = "./#{root}/#{repo_id}/helpers"
         Dir["#{helpers_path}/*.rb"].each do |file|
           helper_id = file.split('/').last.split('.').first
